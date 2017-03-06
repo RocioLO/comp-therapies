631a456e33b9cf2065830528403ed473dd0c7c2b
RocioLO <rociolopezvelazco@gmail.com>
2017-03-06 16:23:23 +0100

    enh: Step 3 completed

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # comp-therapies
 
 Mitk and c++ training code
+
+![Step 3 Render result](/Descargas/3DRender 1-1.png?raw=true)
+
+
--- Step1.cpp
+++ Step1.cpp
@@ -3,6 +3,11 @@
 #include "QmitkRenderWindow.h"
 
 #include <mitkStandaloneDataStorage.h>
+#include <mitkProperties.h>
+#include <mitkRenderingManager.h>
+
+#include <mitkTransferFunction.h>
+#include <mitkTransferFunctionProperty.h>
 
 #include <itksys/SystemTools.hxx>
 #include <QApplication>
@@ -50,9 +55,44 @@ int main(int argc, char* argv[])
   //*************************************************************************
   //Part III: Put the data into the dataStorage (add the node to the DataStorage)
   // Load datanode (eg. many image formats, surface formats, etc.)
-      mitk::IOUtil::Load(argv[i],*datastorage);
+      mitk::StandaloneDataStorage::SetOfObjects::Pointer dataNodes = mitk::IOUtil::Load(argv[i], *datastorage);
+      if (dataNodes->empty())
+      {
+        fprintf(stderr, "Could not open file %s \n\n", argv[i]);
+        exit(2);
+      }
+    mitk::DataNode::Pointer node = dataNodes->at(0);
+  
+ 
+ 
+  //************************************************************************
+  // Part IIIa: we want all the images to be volume rendered
+  //***********************************************************
+  
+  //We first check that the data is an image
+    mitk::Image::Pointer image = dynamic_cast<mitk::Image *>(node->GetData());
+  
+	if (image.IsNotNull())
+	{
+	  
+	  // Set the property "volumerendering" to the Boolean value "true"
+	  node->SetProperty("volumerendering", mitk::BoolProperty::New(true));
+	  // Create a transfer function to assign optical properties to grey-values of the data
+	  mitk::TransferFunction::Pointer tf = mitk::TransferFunction::New();
+	  tf->InitializeByMitkImage(image);
+	   
+	  // Set the color transfer function AddRGBPoint(double x, double r, double g, double b)
+	  tf->GetColorTransferFunction()->AddRGBPoint(tf->GetColorTransferFunction()->GetRange()[0], 1.0, 0.0, 0.0);
+	  tf->GetColorTransferFunction()->AddRGBPoint(tf->GetColorTransferFunction()->GetRange()[1], 1.0, 1.0, 0.0);
+	  
+	  // Set the piecewise opacity transfer function AddPoint(double x, double y)
+	  tf->GetScalarOpacityFunction()->AddPoint(0, 0);
+	  tf->GetScalarOpacityFunction()->AddPoint(tf->GetColorTransferFunction()->GetRange()[1], 1);
+	  
+	  node->SetProperty("TransferFunction", mitk::TransferFunctionProperty::New(tf.GetPointer()));
+	}
   }
-
+  
   //*************************************************************************
   // Part IV: Create window and pass the datastorage to it
   //*************************************************************************
@@ -62,16 +102,25 @@ int main(int argc, char* argv[])
 
   // Tell the RenderWindow which (part of) the datastorage to render
   renderWindow.GetRenderer()->SetDataStorage(datastorage);
-
+  
+  //**************************
+  //Part IIIb
+  //***************************
+  
+  // Use it as a 3D view!
+  renderWindow.GetRenderer()->SetMapperID(mitk::BaseRenderer::Standard3D);
+  // Reposition the camera to include all visible actors
+  renderWindow.GetRenderer()->GetVtkRenderer()->ResetCamera();
+  
   // Initialize the RenderWindow
   mitk::TimeGeometry::Pointer geo = datastorage->ComputeBoundingGeometry3D(datastorage->GetAll());
   mitk::RenderingManager::GetInstance()->InitializeViews( geo );
   //mitk::RenderingManager::GetInstance()->InitializeViews();
 
   // Select a slice
-  mitk::SliceNavigationController::Pointer sliceNaviController = renderWindow.GetSliceNavigationController();
-  if (sliceNaviController)
-    sliceNaviController->GetSlice()->SetPos( 2 );
+  //mitk::SliceNavigationController::Pointer sliceNaviController = renderWindow.GetSliceNavigationController();
+ // if (sliceNaviController)
+    //sliceNaviController->GetSlice()->SetPos( 2 );
 
   //*************************************************************************
   // Part V: Qt-specific initialization
