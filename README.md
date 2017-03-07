10b3d0b79119f97286ce21a57d119339181d3fa4
RocioLO <rociolopezvelazco@gmail.com>
2017-03-07 11:01:42 +0100

    enh: Step 4 completed

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Mitk and c++ training code
 
 
 Image result for Step 3:
+
+
 ![Step 3 Render result](https://github.com/RocioLO/comp-therapies/blob/master/images/3DRender.png)
 
+Image result for Step 4: Axial and Sagittal views
 
+![Step 3 Views result](https://github.com/RocioLO/comp-therapies/blob/master/images/axialSagital.png)
--- Step1.cpp
+++ Step1.cpp
@@ -1,18 +1,18 @@
 
 #include "QmitkRegisterClasses.h"
 #include "QmitkRenderWindow.h"
-
-#include <mitkStandaloneDataStorage.h>
-#include <mitkProperties.h>
-#include <mitkRenderingManager.h>
-
-#include <mitkTransferFunction.h>
-#include <mitkTransferFunctionProperty.h>
-
-#include <itksys/SystemTools.hxx>
-#include <QApplication>
+#include "QmitkSliceWidget.h"
+#include "mitkNodePredicateDataType.h"
+#include "mitkProperties.h"
+#include "mitkRenderingManager.h"
+#include "mitkStandaloneDataStorage.h"
 
 #include <mitkIOUtil.h>
+#include <QApplication>
+#include <QHBoxLayout>
+#include <itksys/SystemTools.hxx>
+#include <mitkImage.h>
+
 
 //##Documentation
 //## @brief Load image (nrrd format) and display it in a 2D view
@@ -48,29 +48,32 @@ int main(int argc, char* argv[])
   int i;
   for (i = 1; i< argc; ++i)
   {
+	  
 	  //For testing
 	  if (strcmp(argv[i], "-testing") == 0)
 	    continue;
-	    
+	  mitk::IOUtil::Load(argv[i], *datastorage);
+  }
   //*************************************************************************
   //Part III: Put the data into the dataStorage (add the node to the DataStorage)
   // Load datanode (eg. many image formats, surface formats, etc.)
-      mitk::StandaloneDataStorage::SetOfObjects::Pointer dataNodes = mitk::IOUtil::Load(argv[i], *datastorage);
+  /* This is to load data and show it in the 3d render  
+   * mitk::StandaloneDataStorage::SetOfObjects::Pointer dataNodes = mitk::IOUtil::Load(argv[i], *datastorage);
       if (dataNodes->empty())
       {
         fprintf(stderr, "Could not open file %s \n\n", argv[i]);
         exit(2);
       }
-    mitk::DataNode::Pointer node = dataNodes->at(0);
+    mitk::DataNode::Pointer node = dataNodes->at(0);*/
   
- 
- 
+     
+   
   //************************************************************************
   // Part IIIa: we want all the images to be volume rendered
   //***********************************************************
   
   //We first check that the data is an image
-    mitk::Image::Pointer image = dynamic_cast<mitk::Image *>(node->GetData());
+   /* mitk::Image::Pointer image = dynamic_cast<mitk::Image *>(node->GetData());
   
 	if (image.IsNotNull())
 	{
@@ -90,23 +93,72 @@ int main(int argc, char* argv[])
 	  tf->GetScalarOpacityFunction()->AddPoint(tf->GetColorTransferFunction()->GetRange()[1], 1);
 	  
 	  node->SetProperty("TransferFunction", mitk::TransferFunctionProperty::New(tf.GetPointer()));
-	}
-  }
+	}*/
+  
   
   //*************************************************************************
-  // Part IV: Create window and pass the datastorage to it
+  // Part IV: Create window and pass the datastorage (tree) to it
   //*************************************************************************
-
+  
+  //Create toplevel widget with horizontal layout
+  QWidget toplevelWidget;
+  QHBoxLayout layout;
+  layout.setSpacing(2);
+  layout.setMargin(0);
+  toplevelWidget.setLayout(&layout);
+  
+  //**********************************
+  // Part IVa: 3D view
+  //*******************************************
+  
   // Create a RenderWindow
-  QmitkRenderWindow renderWindow;
-
+  QmitkRenderWindow renderWindow(&toplevelWidget);
+  layout.addWidget(&renderWindow);
+  
   // Tell the RenderWindow which (part of) the datastorage to render
   renderWindow.GetRenderer()->SetDataStorage(datastorage);
   
-  //**************************
-  //Part IIIb
-  //***************************
+  //use it as a 3D view
+  renderWindow.GetRenderer()->SetMapperID(mitk::BaseRenderer::Standard3D);
   
+  //reposition the camera to include all visible actors
+  renderWindow.GetRenderer()->GetVtkRenderer()->ResetCamera();
+  
+  
+  //Create a 2D view for slicing axially (IVb)
+  QmitkSliceWidget view2(&toplevelWidget);
+  layout.addWidget(&view2);
+  view2.SetLevelWindowEnabled(true);
+  view2.SetDataStorage(datastorage);
+  
+  //Get the image from the data storage
+  mitk::DataStorage::SetOfObjects::ConstPointer rs = datastorage->GetSubset(mitk::TNodePredicateDataType<mitk::Image>::New());
+  
+  view2.SetData(rs->Begin(), mitk::SliceNavigationController::Axial);
+  
+  // We want to see the position of the slice in 2D and the
+  // slice itself in 3D: so we add it to the datastorage.
+  
+  
+  //Create a 2D view for slicing sagitally (IVc)
+  QmitkSliceWidget view3(&toplevelWidget);
+  layout.addWidget(&view3);
+  view3.SetDataStorage(datastorage);
+  
+  view3.SetData(rs->Begin(), mitk::SliceNavigationController::Sagittal);
+  
+  //return qtapplication.exec();
+  
+  datastorage->Add(view2.GetRenderer()->GetCurrentWorldPlaneGeometryNode());
+  datastorage->Add(view3.GetRenderer()->GetCurrentWorldPlaneGeometryNode());
+  
+  //return qtapplication.exec()
+  
+  
+  //**************************
+  //Part IIIb (for step 3)
+  //***************************
+  /*
   // Use it as a 3D view!
   renderWindow.GetRenderer()->SetMapperID(mitk::BaseRenderer::Standard3D);
   // Reposition the camera to include all visible actors
@@ -121,16 +173,18 @@ int main(int argc, char* argv[])
   //mitk::SliceNavigationController::Pointer sliceNaviController = renderWindow.GetSliceNavigationController();
  // if (sliceNaviController)
     //sliceNaviController->GetSlice()->SetPos( 2 );
-
+   */
   //*************************************************************************
   // Part V: Qt-specific initialization
   //*************************************************************************
-  renderWindow.show();
-  renderWindow.resize( 256, 256 );
+  //renderWindow.show();
+  //enderWindow.resize( 256, 256 );
+
+  toplevelWidget.show();
+  
+  //
 
   qtapplication.exec();
-
-
   // cleanup: Remove References to DataStorage. This will delete the object
   datastorage = NULL;
 }
